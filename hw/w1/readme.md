6b425632588daa6fc44ba1ef1f8ca05f710f0a91
jacknguyen <nguyenphucbao098@gmail.com>
2023-10-06 20:23:07 +0700

    init

---
+++ hw/w1/readme.md
@@ -0,0 +1,14 @@
+## Requirements
+
+1. Nodejs
+2. NPM
+3. MySQL: XAMPP
+4. Library: Express, ...
+
+## Steps
+
+1. Setup Environment
+2. Setup Project Template
+3. Add CRUD API - actor
+
+##
---
+++ hw/w1/utils/db.js
@@ -0,0 +1,13 @@
+import knex from 'knex';
+
+export default knex({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'sakila'
+  },
+  pool: { min: 0, max: 10 }
+});
