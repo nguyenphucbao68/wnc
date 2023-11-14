0abe965daed5b92ddb337bfc13f0ed55bf15f370
Tan Sang <btsang20@clc.fitus.edu.vn>
2023-11-15 00:30:38 +0700

    update version 4

--- hw/w6/Version_4/FilmServer/middlewares/verifySecret.js
+++ hw/w6/Version_4/FilmServer/middlewares/verifySecret.js
@@ -1,15 +1,30 @@
-export default function verifySecret(req, res, next) {
-  const secret = req.headers['secret'];
+import crypto from 'crypto';
 
-  if (!secret) {
+export default function verifySecret(req, res, next) {
+  const clientTime = req.headers['time'];
+  const clientHash = req.headers['token'];
+
+  if (!clientTime || !clientHash) {
     return res.status(401).json({
       error: 'Unauthorized',
     });
   }
 
-  if (secret !== process.env.SECRET_KEY) {
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (currentTime - parseInt(clientTime, 10) > 60) {
     return res.status(401).json({
-      error: 'Unauthorized',
+      error: 'Request time expired',
+    });
+  }
+
+  const serverHash = crypto
+    .createHash('sha256')
+    .update(req.url + clientTime + process.env.SECRET_KEY)
+    .digest('hex');
+
+  if (serverHash !== clientHash) {
+    return res.status(401).json({
+      error: 'Invalid token',
     });
   }
 
