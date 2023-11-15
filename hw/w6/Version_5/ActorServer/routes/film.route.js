61305d20086f8d5f6b8f34de133cd6eceb79f0c1
Tan Sang <btsangk20@gmail.com>
2023-11-15 12:46:02 +0700

    update hash for version 5

--- hw/w6/Version_5/ActorServer/routes/film.route.js
+++ hw/w6/Version_5/ActorServer/routes/film.route.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import axios from 'axios';
 import verifyToken from '../middlewares/verifyToken.js';
+import crypto from 'crypto';
 
 const router = express.Router();
 
 router.get('/', verifyToken, async function (req, res) {
   const url = 'http://localhost:3002/api/films';
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  const clientHash = crypto
+    .createHash('sha256')
+    .update(req.url + currentTime + process.env.SECRET_KEY)
+    .digest('hex');
+
   try {
     const response = await axios.get(url, {
       headers: {
-        secret: process.env.SECRET_KEY,
+        time: currentTime,
+        token: clientHash,
       },
     });
 
@@ -22,4 +31,3 @@ router.get('/', verifyToken, async function (req, res) {
 });
 
 export default router;
-
